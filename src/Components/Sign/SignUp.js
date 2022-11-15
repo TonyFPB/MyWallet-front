46ff293df7dbb9c9f7f718f05e060228b883b6b3
Antonio Breyer <antoniofabiopbreyer@gmail.com>
2022-11-15 15:38:36 -0300

    feat: sign-up and connection sign-up to api

---
+++ src/Components/Sign/SignUp.js
@@ -0,0 +1,71 @@
+import { useState } from "react"
+import { StyledFormSign, StyledSign, Tittle } from "./StyledSign"
+import axios from "axios"
+export default function SignUp() {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [passwordConfirm, setPasswordConfirm] = useState('')
+    const [error, setError] = useState("")
+
+    function registering(e) {
+        e.preventDefault()
+        const URL = "http://localhost:5000/sign-up"
+
+        if (password === passwordConfirm) {
+            const body = { name, email, password }
+            const request = axios.post(URL, body)
+            request.then(res => {
+                alert(res.data)
+                setError("")
+            })
+            request.catch(err=>{console.log(err.response);setError("Preencha os dados corretamentes")})
+        }else{
+            setError("Senha incorreta")
+        }
+    }
+
+    return (
+        <StyledSign>
+            <Tittle>MyWallet</Tittle>
+            <StyledFormSign onSubmit={registering}>
+                <input
+                    placeholder="Nome"
+                    value={name}
+                    id="name"
+                    type="text"
+                    onChange={(e) => setName(e.target.value)}
+                    required
+                />
+                <input
+                    placeholder="E-mail"
+                    value={email}
+                    id="email"
+                    type="email"
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                />
+                <input
+                    placeholder="Senha"
+                    value={password}
+                    id="password"
+                    type="password"
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                />
+                <input
+                    placeholder="Cnfirme a senha"
+                    value={passwordConfirm}
+                    id="passwordConfirm"
+                    type="password"
+                    onChange={(e) => setPasswordConfirm(e.target.value)}
+                    required
+                />
+                <button>Cadastrar</button>
+            </StyledFormSign>
+            <p>{error}</p>
+            <a>Já tem uma conta? Entre agora!</a>
+        </StyledSign>
+    )
+}
+
---
+++ src/Components/Sign/StyledSign.js
@@ -0,0 +1,73 @@
+import styled from "styled-components"
+
+const StyledSign = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    width: 100%;
+    a{
+        font-family: 'Raleway';
+        font-style: normal;
+        font-weight: 700;
+        font-size: 15px;
+        color: #FFFFFF;
+        margin-top: 32px;
+    }
+    p{
+        color: red;
+        margin-top: 10px;
+        font-size: 20px;
+        font-family: 'Raleway';
+        font-style: normal;
+        font-weight: 400;
+    }
+`
+const Tittle = styled.h1`
+    font-family: 'Saira Stencil One', cursive;
+    font-weight: 400;
+    font-size: 32px;
+    margin-bottom: 24px;
+    color: #FFFFFF;
+`
+const StyledFormSign = styled.form`
+    width: 90%;
+    margin: 0px 24px 0px 24px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    input{
+        width: 100%;
+        height: 58px;
+        background: #FFFFFF;
+        border-radius: 5px;
+        margin: 5px 0px 5px 0px;
+        font-family: 'Raleway';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 20px;
+        ::placeholder{
+            color: #000000;
+            font-family: 'Raleway';
+            font-style: normal;
+            font-weight: 400;
+            font-size: 20px;
+        }
+    }
+    button{
+        background-color: #A328D6;
+        width: 100%;
+        height: 46px;
+        border-radius: 5px;
+        border-style: none;
+        font-family: 'Raleway';
+        font-style: normal;
+        font-weight: 700;
+        font-size: 20px;
+        color: #FFFFFF;
+        margin-top: 8px;
+    }
+`
+
+export { StyledSign, Tittle, StyledFormSign }
